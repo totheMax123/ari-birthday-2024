46e4ec74fa875907cd56bb9557c2772763df7d92
totheMax123 <mdickman@stillbright.co>
2024-10-30 21:16:15 -0400

    bug fixes

--- password_validation.js
+++ password_validation.js
@@ -1,5 +1,5 @@
 function validate_password() {
-    let x = document.forms["password input"]["datepick"].value;
+    let x = document.forms["password_input"]["datepick"].value;
     console.log(x);
     /*if (x != "") {
         alert("Incorrect password. Try again. ");
