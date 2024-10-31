00ed1f173a23342660fe4359e4e2ecff44c05f0c
totheMax123 <mdickman@stillbright.co>
2024-10-30 23:45:14 -0400

    test string function

--- select_from_file.js
+++ select_from_file.js
@@ -1,7 +1,9 @@
 function select_ariism() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        document.getElementById("main_content").innerHTML = this.responseText;
+        let full_text = this.responseText;
+        let phrase = full_text.slice(3);
+        document.getElementById("main_content").innerHTML = phrase;
     }
     xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/ariisms.txt", true);
     xhttp.send();
