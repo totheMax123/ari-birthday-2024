0e849b18d0562ae8ef2b4d57a6cfed7e767212b8
totheMax123 <mdickman@stillbright.co>
2024-10-30 20:52:35 -0400

    see what password looks like in javascript

---
+++ password_validation.js
@@ -0,0 +1,8 @@
+function validate_password() {
+    let x = document.forms["password input"]["datepick"].value;
+    console.log(x);
+    /*if (x != "") {
+        alert("Incorrect password. Try again. ");
+        return false;
+    }*/
+}
