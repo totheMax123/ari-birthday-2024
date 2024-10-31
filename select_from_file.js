817b715c82c713d746d38f044aa94ccdf9fab0c1
totheMax123 <mdickman@stillbright.co>
2024-10-30 23:51:49 -0400

    randomly select a phrase :)

--- select_from_file.js
+++ select_from_file.js
@@ -3,7 +3,7 @@ function select_ariism() {
     xhttp.onload = function() {
         let full_text = this.responseText;
         let phrase_array = full_text.split("\n");
-        let phrase = phrase_array[0];
+        let phrase = phrase_array[Math.floor(Math.random() * (phrase_array.length - 1))];
         document.getElementById("main_content").innerHTML = phrase;
     }
     xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/ariisms.txt", true);
