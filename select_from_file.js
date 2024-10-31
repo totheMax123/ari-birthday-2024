4b23e1976f53bb9c0c37f90db0c0b335893aa017
totheMax123 <mdickman@stillbright.co>
2024-10-30 22:47:09 -0400

    test new file reading mechanism

--- select_from_file.js
+++ select_from_file.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
+
 function select_ariism() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        document.getElementById("main_content").innerHTML = this.responseText;
-        // console.log(this.responseText);
-    }
-    xhttp.open("GET", "/assets/ariisms.txt", true);
-    xhttp.send();
+    fs.readFile("assets/ariisms.txt", (err, data) => {
+        if (err) throw err;
+      
+        console.log(data.toString());
+      });
+    
 }
