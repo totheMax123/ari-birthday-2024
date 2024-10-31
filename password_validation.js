d03472f0f6a7bfd877a6c573bf0bf4ab0796c9d2
totheMax123 <mdickman@stillbright.co>
2024-10-30 21:29:02 -0400

    getting better at web development lmao

--- password_validation.js
+++ password_validation.js
@@ -1,8 +1,7 @@
 function validate_password() {
     let x = document.forms["password_input"]["datepick"].value;
-    console.log(x);
-    /*if (x != "") {
+    if (x != "2001-11-01") {
         alert("Incorrect password. Try again. ");
         return false;
-    }*/
+    }
 }
