49a9c7d4bf2fbcf8cee3b18232d7d957937720c9
totheMax123 <mdickman@stillbright.co>
2024-10-30 23:47:42 -0400

    test splitting string

--- select_from_file.js
+++ select_from_file.js
@@ -2,7 +2,8 @@ function select_ariism() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         let full_text = this.responseText;
-        let phrase = full_text.slice(3);
+        let phrase_array = full_text.split("\n");
+        let phrase = phrase_array[0];
         document.getElementById("main_content").innerHTML = phrase;
     }
     xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/ariisms.txt", true);
