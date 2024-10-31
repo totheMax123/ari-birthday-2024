ee4c1e5e344464725535dde417145f8aff113c54
totheMax123 <mdickman@stillbright.co>
2024-10-31 18:26:56 -0400

    fix all the buttons??? and make them actually go somewhere

--- select_from_file.js
+++ select_from_file.js
@@ -9,3 +9,39 @@ function select_ariism() {
     xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/ariisms.txt", true);
     xhttp.send();
 }
+
+function select_maxism() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        let full_text = this.responseText;
+        let phrase_array = full_text.split("\n");
+        let phrase = phrase_array[Math.floor(Math.random() * (phrase_array.length - 1))];
+        document.getElementById("main_content").innerHTML = phrase;
+    }
+    xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/maxisms.txt", true);
+    xhttp.send();
+}
+
+function select_date_idea() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        let full_text = this.responseText;
+        let phrase_array = full_text.split("\n");
+        let phrase = phrase_array[Math.floor(Math.random() * (phrase_array.length - 1))];
+        document.getElementById("main_content").innerHTML = "We should " + phrase + "!";
+    }
+    xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/date_ideas.txt", true);
+    xhttp.send();
+}
+
+function select_compliment() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        let full_text = this.responseText;
+        let phrase_array = full_text.split("\n");
+        let phrase = phrase_array[Math.floor(Math.random() * (phrase_array.length - 1))];
+        document.getElementById("main_content").innerHTML = phrase;
+    }
+    xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/compliment.txt", true);
+    xhttp.send();
+}
