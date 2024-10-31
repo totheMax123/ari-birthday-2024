db17871668606d7234feac2db52d860e93d837fd
totheMax123 <mdickman@stillbright.co>
2024-10-30 23:06:17 -0400

    ????

--- select_from_file.js
+++ select_from_file.js
@@ -1,7 +1,7 @@
 function select_ariism() {
     var txtFile = new XMLHttpRequest();
     txtFile.open("GET", "http://tothemax123.github.io/assests/ariisms.txt", true);
-    txtFile.onreadystatechange = function() {
+    txtFile.onload = function() {
       if (txtFile.readyState === 4 && txtFile.status == 200) {
          allText = txtFile.responseText;
       }
