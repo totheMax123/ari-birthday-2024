ed09e2194f8cbdb8657e6b1239b082ee769722d3
totheMax123 <mdickman@stillbright.co>
2024-10-30 23:12:31 -0400

    stupid stupid

--- select_from_file.js
+++ select_from_file.js
@@ -1,7 +1,7 @@
 function select_ariism() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("main_content").innerHTML = this.responseText;
     }
     xhttp.open("GET", "https://tothemax123.github.io/ari-birthday-2024/assets/ariisms.txt", true);
     xhttp.send();
